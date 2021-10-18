05180650e8b1743d5f66c5c9d1d30fb7011cb142
Andrien Wiandyano <arnnied03@gmail.com>
2021-10-18 09:39:24 +0700

    feat: added `neighbour_of_filled()` function

--- player.py
+++ player.py
@@ -37,8 +37,62 @@ class Human(Player):
 
 
 class Computer(Player):
-    # def opponent_sibling_filled()
-    # def sibling_filled(self, board: list, board_size: int) -> list:
+    def neighbour_of_filled(self, board: list, board_size: int) -> list:
+        filled_slot = self.filled_slot(board, board_size)
+        board_full_size = board_size * board_size
+
+        neighbour = set()
+        for filled in filled_slot:
+            y_up = filled - board_size
+            y_up_left = y_up - 1
+            y_up_right = y_up + 1
+
+            y_down = filled + board_size
+            y_down_left = y_down - 1
+            y_down_right = y_down + 1
+
+            if y_up >= 0:
+                neighbour.add(y_up)
+
+            if y_up_left >= 0 and row_convert(y_up, board_size)[1] - 1 >= 0:
+                neighbour.add(y_up_left)
+
+            if y_up_right >= 0 and row_convert(y_up, board_size)[1] + 1 < board_size:
+                neighbour.add(y_up_right)
+
+            if y_down < board_full_size:
+                neighbour.add(y_down)
+
+            if (
+                y_down_left < board_full_size
+                and row_convert(y_down, board_size)[1] - 1 >= 0
+            ):
+                neighbour.add(y_down_left)
+
+            if (
+                y_down_right < board_full_size
+                and row_convert(y_down, board_size)[1] + 1 < board_size
+            ):
+                neighbour.add(y_down_right)
+
+            x = filled
+            x_left = filled - 1
+            x_right = filled + 1
+
+            if x_left >= 0 and row_convert(x, board_size)[1] - 1 >= 0:
+                neighbour.add(x_left)
+
+            if (
+                x_right < board_full_size
+                and row_convert(x, board_size)[1] + 1 < board_size
+            ):
+                neighbour.add(x_left)
+
+        collide = neighbour & set(filled_slot)
+        for i in collide:
+            neighbour.remove(i)
+
+        return neighbour
 
     def filled_slot(self, board: list, board_size: int) -> list:
         """Return all filled slot on the board"""
@@ -67,6 +121,8 @@ class Computer(Player):
 
         random_choice = floor(uniform(0, len(empty_slot)))
         choice = empty_slot[random_choice]
+
+        self.neighbour_of_filled(board, board_size)
 
         print(f"> {choice + 1}")
 
