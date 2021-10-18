e9f68e23610802824c9a0404e3f0593bc0771289
Andrien Wiandyano <arnnied03@gmail.com>
2021-10-18 09:45:19 +0700

    feat(utils): added `rng()` function to `utils.py`

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
+from random import random
+
+
 def row_convert(data: int, board_size: int) -> tuple:
     """Convert raw slot position to a value to access the board 2d array"""
 
     row, col = divmod(data, board_size)
 
     return row, col
+
+
+def rng(data: float = None) -> float:
+    if data != None:
+        return data >= random()
+
+    return random()
